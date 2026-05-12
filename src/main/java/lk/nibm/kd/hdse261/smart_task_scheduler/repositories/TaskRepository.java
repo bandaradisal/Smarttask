d01e092d7a32c7396a082da1fedd50a5d22de27d
S.M.A.M ABEYSINGHE <aabeysinghe0@gmail.com>
2026-05-12 11:10:12 +0530

    Added TaskController and implemented TaskService CRUD APIs

---
+++ src/main/java/lk/nibm/kd/hdse261/smart_task_scheduler/repositories/TaskRepository.java
@@ -0,0 +1,7 @@
+package lk.nibm.kd.hdse261.smart_task_scheduler.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository {
+
+}
