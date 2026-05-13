cc81967050c2430a24922c5fe037e52f44c79e7f
R.ROHITH <rrravikumarrohith@gmail.com>
2026-05-13 21:01:06 +0530

    feat: update frontend UI with improvements and new components

--- src/main/java/lk/nibm/kd/hdse261/smart_task_scheduler/controllers/TaskController.java
+++ src/main/java/lk/nibm/kd/hdse261/smart_task_scheduler/controllers/TaskController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tasks")
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/tasks")
 public class TaskController {
 
     private final TaskService taskService;
