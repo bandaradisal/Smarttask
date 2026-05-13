b3cda6e0ad5b27020c105d93a64e6dacffc9ed62
S.M.A.M ABEYSINGHE <aabeysinghe0@gmail.com>
2026-05-13 22:37:20 +0530

    added validation

--- src/main/java/lk/nibm/kd/hdse261/smart_task_scheduler/services/UserService.java
+++ src/main/java/lk/nibm/kd/hdse261/smart_task_scheduler/services/UserService.java
@@ -1,17 +1,18 @@
 package lk.nibm.kd.hdse261.smart_task_scheduler.services;
 
 import lk.nibm.kd.hdse261.smart_task_scheduler.repositories.TaskRepository;
+import lk.nibm.kd.hdse261.smart_task_scheduler.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private TaskRepository repository;
+    private UserRepository repository;
 
     @Autowired
-    public UserService(TaskRepository repository){
+    public UserService(UserRepository repository){
         this.repository = repository;
     }
 
-    
+    //Register
 }
